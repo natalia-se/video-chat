f737150038ba2fd77b54a6ab93b39817e972f448
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2023-05-04 14:15:11 +0200

    Create empty pages

---
+++ frontend/src/pages/InCallPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InCallPage = () => {
+  return <div>In call page</div>;
+};
+
+export default InCallPage;
---
+++ frontend/src/pages/VideoChatPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoChatPage = () => {
+  return <div>Video Chat Page</div>;
+};
+
+export default VideoChatPage;
