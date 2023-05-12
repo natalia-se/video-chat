8139e4e45f6afd628431f8dad3b95bfea8e4a8c0
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2023-05-12 14:59:43 +0200

    Add reducer for video rooms

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import chatReducer from "./reducers/chatSlice";
+import videoRoomsReducer from "./reducers/videoRoomSlice";
 
 const store = configureStore({
   reducer: {
     chat: chatReducer,
+    videoRooms: videoRoomsReducer,
   },
 });
 
