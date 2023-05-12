5d96a429517fece0d1f493144b4f4b5afc1a38e8
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2023-05-12 15:08:32 +0200

    Listen to rooms and signaling about room creating

--- frontend/src/socketConnection.js
+++ frontend/src/socketConnection.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
 import { onlineUsersHandler } from "./app/actions/userActions";
+import { videoRoomsListHandler } from "./app/actions/videoRoomActions";
 
 let socket = null;
 
@@ -13,8 +14,16 @@ export const connectWithSocketIOServer = () => {
   socket.on("online-users", (usersData) => {
     onlineUsersHandler(socket.id, usersData);
   });
+  socket.on("video-rooms", (videoRooms) => {
+    videoRoomsListHandler(videoRooms);
+  });
 };
 
 export const login = (data) => {
   socket.emit("user-login", data);
+};
+
+export const createVideoRoom = (data) => {
+  console.log("emitting");
+  socket.emit("video-room-create", data);
 };
