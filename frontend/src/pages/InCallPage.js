ef01b7e033928ae237dd9869253c6a6340783809
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2023-05-12 15:47:21 +0200

    Add video to inCallPage

--- frontend/src/pages/InCallPage.js
+++ frontend/src/pages/InCallPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Video from "../components/Video";
 
 const InCallPage = () => {
-  return <div>In call page</div>;
+  const localStream = useSelector((state) => state.videoRooms.localStream);
+  const remoteStream = useSelector((state) => state.videoRooms.remoteStream);
+  return (
+    <div className="w-full h-screen bg-top bg-cover bg-blue-950">
+      <div className="flex flex-col items-centers md:flex-row">
+        {/* Camera, mic, leave buttons */}
+        {localStream && <Video stream={localStream} muted />}
+        {remoteStream && <Video stream={remoteStream} muted />}
+      </div>
+    </div>
+  );
 };
 
 export default InCallPage;
